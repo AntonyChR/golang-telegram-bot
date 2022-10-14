192306d83c2f206e93933ef43f571aef617aa7e6
AntonyChR <antonychr99@gmail.com>
2022-10-14 11:41:05 -0500

    add: telegram api client

---
+++ bot/TelegramApi.go
@@ -0,0 +1,41 @@
+package bot
+
+import (
+	"bytes"
+	"fmt"
+)
+
+type TelegramApi struct {
+	BaseUrl   string
+	EndPoints map[string]string
+	ServerUrl string
+}
+
+func (t *TelegramApi) registerWebhook() error {
+	resp, err := post(t.BaseUrl+"/setWebhook?url="+t.ServerUrl, "application/json", nil)
+	fmt.Println(resp)
+	return err
+}
+func (t *TelegramApi) SetCommandsDescription(desc map[string]string) error{
+	if len(desc) == 0 {
+		return nil
+	}
+	fmt.Println("[i] Adding command description")
+	objBytes := commandDesc2json(desc)
+	resp,err := post(t.BaseUrl+"/setMyCommands", "application/json", bytes.NewBuffer(objBytes))
+	fmt.Println(resp)
+	return err
+}
+
+func (t *TelegramApi) removeWebhook() error {
+	resp, err := post(t.BaseUrl+"/setWebhook?remove", "application/json", nil)
+	fmt.Println(resp)
+	return err
+}
+
+func (t *TelegramApi) SendText(text string) {
+
+}
+
+func (t *TelegramApi) SendFile(endPoint string, fileType string, fileContent []byte) {
+}
