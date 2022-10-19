fa0a8d2a68a9c7c6a24d6848bf4a8a774afa71de
AntonyChR <antonychr99@gmail.com>
2022-10-19 16:14:28 -0500

    remove command

--- bot/lib.go
+++ bot/lib.go
@@ -1,31 +1,10 @@
 package bot
 
 import (
-	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 )
-type commands struct {
-	Commands []commandDesc `json:"commands"` 
-}
-
-type commandDesc struct{
-	Command     string `json:"command"`
-	Description string `json:"description"`
-}
-
-func commandDesc2json(m map[string]string) []byte{
-	var cmds []commandDesc
-	for k, v := range m {
-		cmds = append(cmds,commandDesc{Command: k, Description: v}) 
-	}
-	obj := commands{
-		Commands: cmds,
-	}
-	objBytes, _ := json.Marshal(obj)
-	return objBytes
-}
 
 func post(url string, contentType string, content io.Reader) (string, error) {
 	fmt.Println(url)
