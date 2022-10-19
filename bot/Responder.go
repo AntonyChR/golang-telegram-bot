8f279b67415c1883d8597534347a875743861a1c
AntonyChR <antonychr99@gmail.com>
2022-10-19 16:11:10 -0500

    add: Reply method

--- bot/Responder.go
+++ bot/Responder.go
@@ -1,15 +1,26 @@
 package bot
 
-type Responder struct{
+type Responder struct {
 	apiService *ApiClient
 }
 
-
-type Content struct{
+type Content struct {
 	Text string
 	Type string
 	Data []byte
 }
 
-func (r *Responder) Reply(m Message,c Content){
+func (r *Responder) Reply(m Message, c Content) {
+	if c.Type == "" || c.Type == "text" {
+		data := TextMessage{
+			ChatID:           m.Chat.ID,
+			ReplyToMessageID: m.MessageID,
+			Text:             c.Text,
+		}
+		r.apiService.SendText(data)
+	}
+}
+
+func (r *Responder) SendToChat(chatId int, c Content) {
+	r.apiService.SendText(TextMessage{ChatID: chatId, Text: c.Text})
 }
