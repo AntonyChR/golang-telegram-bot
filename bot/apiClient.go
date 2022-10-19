ddef7e53c777fe2b0d9e3d1d7a723f63ce8d52e9
AntonyChR <antonychr99@gmail.com>
2022-10-19 10:03:20 -0500

    add: TextMessage struct

--- bot/apiClient.go
+++ bot/apiClient.go
@@ -2,8 +2,15 @@ package bot
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 )
+
+type TextMessage struct {
+	ChatID           int    `json:"chat_id"`
+	ReplyToMessageID int    `json:"reply_to_message_id"`
+	Text             string `json:"text"`
+}
 
 type ApiClient struct {
 	BaseUrl   string
@@ -12,14 +19,14 @@ type ApiClient struct {
 }
 
 func (t *ApiClient) RegisterWebhook() error {
-	resp, err := post(t.BaseUrl+"/setWebhook?url="+t.ServerUrl, "application/json", nil)
+	resp, err := post(t.BaseUrl+t.EndPoints["setWebhook"]+t.ServerUrl, "application/json", nil)
 	fmt.Println(resp)
 	return err
 }
 
 func (t *ApiClient) RemoveWebhook() error {
 	fmt.Println("[!] Removing webhook...")
-	resp, err := post(t.BaseUrl+"/setWebhook?remove", "application/json", nil)
+	resp, err := post(t.BaseUrl+t.EndPoints["removeWebhook"], "application/json", nil)
 	fmt.Println(resp)
 	return err
 }
@@ -30,13 +37,14 @@ func (t *ApiClient) SetCommandsDescription(desc map[string]string) error {
 	}
 	fmt.Println("[i] Adding command description")
 	objBytes := commandDesc2json(desc)
-	resp, err := post(t.BaseUrl+"/setMyCommands", "application/json", bytes.NewBuffer(objBytes))
+	resp, err := post(t.BaseUrl+t.EndPoints["setCommands"], "application/json", bytes.NewBuffer(objBytes))
 	fmt.Println(resp)
 	return err
 }
 
-func (t *ApiClient) SendText(text string) error {
-	_, err := post(t.BaseUrl+"/sendText", "application/json", bytes.NewBuffer([]byte(text)))
+func (t *ApiClient) SendText(data TextMessage) error {
+	dataBytes, _ := json.Marshal(data)
+	_, err := post(t.BaseUrl+t.EndPoints["sendText"], "application/json", bytes.NewBuffer([]byte(dataBytes)))
 	return err
 }
 
