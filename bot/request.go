a630be33b962276d3aa3a2769e1de5310bc6c593
AntonyChR <antonychr99@gmail.com>
2022-10-18 22:31:21 -0500

    add: Message fields

--- bot/request.go
+++ bot/request.go
@@ -6,11 +6,14 @@ type Body struct {
 }
 
 type Message struct {
-	MessageID int    `json:"message_id"`
-	From      From   `json:"from"`
-	Chat      Chat   `json:"chat"`
-	Date      int    `json:"date"`
-	Text      string `json:"text"`
+	MessageID int      `json:"message_id"`
+	From      From     `json:"from"`
+	Chat      Chat     `json:"chat"`
+	Date      int      `json:"date"`
+	Text      string   `json:"text"`
+	Photo     []Photo  `json:"photo"`
+	Caption   string   `json:"caption"`
+	Entities  []Entity `json:"entities"`
 }
 
 type Chat struct {
@@ -24,4 +27,18 @@ type From struct {
 	IsBot        bool   `json:"is_bot"`
 	FirstName    string `json:"first_name"`
 	LanguageCode string `json:"language_code"`
+}
+
+type Photo struct {
+	FileID       string `json:"file_id"`
+	FileUniqueID string `json:"file_unique_id"`
+	FileSize     int64  `json:"file_size"`
+	Width        int64  `json:"width"`
+	Height       int64  `json:"height"`
+}
+
+type Entity struct {
+	Offset int64  `json:"offset"`
+	Length int64  `json:"length"`
+	Type   string `json:"type"`
 }
