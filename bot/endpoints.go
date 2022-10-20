780c03e3deaabcabe439c7b1b7b6eacdda28aea9
AntonyChR <antonychr99@gmail.com>
2022-10-20 12:48:51 -0500

    add: file endpoints

--- bot/endpoints.go
+++ bot/endpoints.go
@@ -4,11 +4,13 @@ func endpoints() map[string]string {
 	return map[string]string{
 		"setWebhook":    "/setWebhook?url=",
 		"removeWebhook": "/setWebhook?remove",
-		"setCommands":   "/setMyCommands",
-		"sendText":      "/sendMessage",
-		"sendVideo":     "",
-		"sendImg":       "",
-		"sendAudio":     "",
-		"sendDoc":       "",
+
+		"setCommands": "/setMyCommands",
+
+		"sendText":  "/sendMessage",
+		"sendVideo": "/sendDocument",
+		"sendImg":   "/sendPhoto",
+		"sendAudio": "/sendAudio",
+		"sendDoc":   "/sendDocument",
 	}
 }
