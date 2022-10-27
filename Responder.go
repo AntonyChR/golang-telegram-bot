03ac4e561bd64f9f296fdd898eb64b5417037ad8
AntonyChR <antonychr99@gmail.com>
2022-10-27 17:19:20 -0500

    add: ban manage chat member

--- Responder.go
+++ Responder.go
@@ -2,7 +2,10 @@ package gtb
 
 import (
 	"errors"
+	"regexp"
+	"strconv"
 	"strings"
+	"time"
 )
 
 type Responder struct {
@@ -45,9 +48,65 @@ func (r *Responder) sendMessage(textFields TextMessage, c Msg) {
 
 }
 
-// Download and save file in:
+// Ban chat member
+// until can be specified with the following formats
 //
-//	dir/{file_type}-{file_name}-{file_unique_id}.{ext}
+//	minutes: "1m", "2m",..."nm"
+//	hours: "1h", "2h",..."nh"
+//	until specific date: "dd/mm/yy"
+//
+// If user is banned for more than 366 days or less than 30 seconds from the
+// current time they are considered to be banned forever.
+// https://core.telegram.org/bots/api#banchatmember
+func (r *Responder) Ban(chaId int, userId int, until string) error {
+
+	minutesRegex := regexp.MustCompile(`[1-9]{1,}m`)
+	hoursRegex := regexp.MustCompile(`[1-9]{1,}h`)
+	dayRegex := regexp.MustCompile(`2[0-9]{3}-(0[1-9]|1[1-2])-(0[1-9]|[1-2][0-9]|3[0-1])`)
+
+	var unixDate int64
+
+	switch {
+	case minutesRegex.MatchString(until):
+		timeStr := strings.ReplaceAll("m", until, "")
+		timeInt, _ := strconv.Atoi(timeStr)
+		unixDate = time.Now().Add(time.Duration(timeInt) * time.Minute).Unix()
+
+	case hoursRegex.MatchString(until):
+		timeStr := strings.ReplaceAll("h", until, "")
+		timeInt, _ := strconv.Atoi(timeStr)
+		unixDate = time.Now().Add(time.Duration(timeInt) * time.Hour).Unix()
+
+	case dayRegex.MatchString(until):
+		t, _ := time.Parse("02/01/2006", until)
+		unixDate = t.Unix()
+
+	default:
+		return errors.New("invalid time format")
+	}
+
+	userData := map[string]any{
+		"chat_id":    chaId,
+		"user_id":    userId,
+		"until_date": unixDate,
+	}
+	err := r.apiService.banChatMember(userData)
+
+	return err
+}
+
+func (r *Responder) Unban(chaId int, userId int) error {
+	userData := map[string]int{
+		"chat_id": chaId,
+		"user_id": userId,
+	}
+	err := r.apiService.unbanChatMember(userData)
+	return err
+}
+
+// Download and save file in dir with format:
+//
+// /{file_type}-{file_name}-{file_unique_id}.{ext}
 func (r *Responder) DownloadFile(fileId string, dir string) error {
 	file := r.apiService.GetFileInfo(fileId)
 
