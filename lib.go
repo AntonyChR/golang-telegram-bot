aca0e981696a7611ad26c29d7955215905d40d2f
AntonyChR <antonychr99@gmail.com>
2022-10-27 23:09:31 -0500

    ref: remove unnecessary function

--- lib.go
+++ lib.go
@@ -1,31 +1,9 @@
 package gtb
 
 import (
-	"encoding/json"
 	"io"
 	"net/http"
 )
-
-type commands struct {
-	Commands []commandDesc `json:"commands"`
-}
-
-type commandDesc struct {
-	Command     string `json:"command"`
-	Description string `json:"description"`
-}
-
-func commandDesc2json(m map[string]string) []byte {
-	var cmds []commandDesc
-	for k, v := range m {
-		cmds = append(cmds, commandDesc{Command: k, Description: v})
-	}
-	obj := commands{
-		Commands: cmds,
-	}
-	objBytes, _ := json.Marshal(obj)
-	return objBytes
-}
 
 func postJSON(url string, content io.Reader) ([]byte, error) {
 
