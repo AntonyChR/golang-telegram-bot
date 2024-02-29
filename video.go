a627b75ea3a4afcbc5ae1467b4d457b59fb5d429
AntonyChR <antonychr99@gmail.com>
2024-02-28 22:46:41 -0500

    add: get thumbnail video

--- video.go
+++ video.go
@@ -3,31 +3,36 @@ package gtb
 import (
 	"fmt"
 	"os/exec"
+	"path/filepath"
 	"strconv"
 	"strings"
 )
 
+func GetThumbnailVideo(videoPath string) (string, error) {
+	basePath := strings.TrimSuffix(filepath.Base(videoPath), filepath.Ext(videoPath))
+	imagePath := basePath + ".jpg"
+	command := fmt.Sprintf("ffmpeg -i %s -vframes 1 -an -s 320x320 -ss 0 %s", videoPath, imagePath)
+	cmd := exec.Command("bash", "-c", command)
+	err := cmd.Run()
 
-func GetThumbnailVideo(path string, t string) (pathImage string) {
-	command := fmt.Sprintf("ffmpeg -i %s -ss %s -vframes 1 thumbnail.jpg", path, t)
-	return command
+	if err != nil {
+		return "", err
+	}
+	return imagePath, nil
 }
 
-func GetVideoDuration(path string) (int, error){
+func GetVideoDuration(path string) (int, error) {
 	command := fmt.Sprintf("ffprobe -i %s -show_entries format=duration -v quiet -of csv=\"p=0\"", path)
-	out, err:= exec.Command("bash", "-c", command).Output()
+	out, err := exec.Command("bash", "-c", command).Output()
 
-	if err != nil{
+	if err != nil {
 		return 0, nil
 	}
-	durationStr:= strings.ReplaceAll(string(out), "\n", "")
+	durationStr := strings.ReplaceAll(string(out), "\n", "")
 
 	dFloat, err := strconv.ParseFloat(durationStr, 64)
-	if err != nil{
+	if err != nil {
 		return 0, nil
 	}
-
-
-	return int(dFloat), nil 
-	
+	return int(dFloat), nil
 }
